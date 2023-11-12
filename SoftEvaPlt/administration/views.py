b6aaa0c0977985fe13a989e57f51ac810b4f5038
lostecho187 <lostecho3613@gmail.com>
2023-11-12 21:39:04 +0800

    page and search

--- SoftEvaPlt/administration/views.py
+++ SoftEvaPlt/administration/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Max
 from .models import SafetyIndicator, Scene
-from datetime import datetime
+from datetime import datetime,timedelta
 import math
 
 
@@ -10,8 +10,9 @@ def admin_view(request):
     return render(request, 'admin.html')
 
 def indicators_view(request):
+    all_si = SafetyIndicator.objects.all()
+    creaiton_flag = 0
     if request.method == 'POST':
-
         if request.POST.get('action') == 'create':
             if len(SafetyIndicator.objects.all()) == 0:
                 id = 1
@@ -23,10 +24,11 @@ def indicators_view(request):
                                             si_state = 1,
                                             si_creator = 'admin',
                                             si_create_time = datetime.now())
+            creaiton_flag = 1
             
         if request.POST.get('action') == 'delete':
-            if len(SafetyIndicator.objects.all()) == 1 & len(Scene.objects.all()) > 0:
-                return render(request, 'indicators.html', {'si_queryset': SafetyIndicator.objects.all(),
+            if len(SafetyIndicator.objects.all()) == 1 and len(Scene.objects.all()) > 0:
+                return render(request, 'indicators.html', {'si_queryset': all_si,
                                                             'delete_failed':'True'})
             SafetyIndicator.objects.filter(si_id = request.POST.get('id')).delete()
 
@@ -45,18 +47,50 @@ def indicators_view(request):
                 si_state = 0
             )
 
+        #进行操作后更新indicators列表
+        all_si = SafetyIndicator.objects.all()
+
+        if request.POST.get('action') == 'search':
+            state = ''
+            if request.POST.get('state') == '已启用':
+                state = '1'
+            if request.POST.get('state') == '已禁用':
+                state = '0'
+            if request.POST.get('time') == "全部":
+                all_si = SafetyIndicator.objects.filter(si_category__contains = request.POST.get('category'),
+                                                    si_name__contains = request.POST.get('name'),
+                                                    si_creator__contains = request.POST.get('creator'),
+                                                    si_state__contains = state)
+            else:
+                time = datetime.now()
+                if request.POST.get('time') == '近一天':
+                    time -= timedelta(days=1)
+                if request.POST.get('time') == '近一周':
+                    time -= timedelta(weeks=1)
+                if request.POST.get('time') == '近一月':
+                    time -= timedelta(days=30)
+                if request.POST.get('time') == '近一年':
+                    time -= timedelta(days=365)
+                print(time)
+                all_si = SafetyIndicator.objects.filter(si_category__contains = request.POST.get('category'),
+                                                        si_name__contains = request.POST.get('name'),
+                                                        si_creator__contains = request.POST.get('creator'),
+                                                        si_state__contains = state,
+                                                        si_create_time__gte = time)
+
     page_len = 4 #页长
-    all_si = SafetyIndicator.objects.all()
-    total = all_si.count()
+    
+    page_max = math.ceil(all_si.count() / page_len)
     page = request.GET.get('page')
     if page == None:
         page = 1
     else:
         page = int(page)
+    if creaiton_flag or page > page_max:# 创建新指标后自动跳转最后一页 或搜索后页数变少也跳至最后一页
+        page = page_max
     all_si =  all_si[(page - 1) * page_len : page * page_len] #将指定页码所包含的条目分出来
 
     pre,next = 2,2
-    page_max = math.ceil(total / page_len)
     last_page = page - 1
     next_page = page + 1
     if(last_page == 0):
@@ -64,6 +98,7 @@ def indicators_view(request):
     if(next_page == page_max + 1):
         next_page = None
     pages = range(max(page - pre, 1),min(page + next, page_max) + 1)
+
 
     return render(request, 'indicators.html', {'si_queryset': all_si, 
                                                'delete_failed':'False',
